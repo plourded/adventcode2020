5f59f3c348ebc04c7e17e645311924ba769cd52f
Dominic Plourde <plourde.dominic@gmail.com>
2020-12-01 19:57:41 -0500

    cleanup

--- src/Command/Puzzle202001Command.php
+++ src/Command/Puzzle202001Command.php
@@ -40,7 +40,7 @@ class Puzzle202001Command extends Command
 
         $output->writeln('Item 1: ' . $item1 . " and item 2: " . $item2);
         $output->writeln('Puzzle 1A response: ' . ($item1 * $item2));
-        
+
         $account_items = $input_file->toArray();;
         $item3 = 0;
 
@@ -53,29 +53,19 @@ class Puzzle202001Command extends Command
                 $temp_items[] = $item2;
 
                 if (($item1 + $item2) < 2020) {
-                    $output->writeln($item1 . " + " . $item2 . " < 2020");
-
                     foreach ($account_items as $acount_item2) {
                         if (($item1 + $item2 + $acount_item2) === 2020) {
-                            $output->writeln(($item1 + $item2 + $acount_item2) . " = 2020");
                             $item3 = $acount_item2;
                             break;
                         }
                     }
                 }
 
-                $output->writeln(count($account_items));
-
             } while (count($account_items) && ($item3===0));
 
             $account_items = $temp_items;
 
-            $output->writeln(count($account_items));
-
-            if (!count($account_items)) {
-                break;
-            }
-        } while (($item1 + $item2 + $item3) !== 2020);
+        } while (($item1 + $item2 + $item3) !== 2020 || !count($account_items));
 
         $output->writeln('Item 1: ' . $item1 . " and item 2: " . $item2 . " and item 3: " . $item3);
         $output->writeln('Puzzle 1B response: ' . ($item1 * $item2 * $item3));
